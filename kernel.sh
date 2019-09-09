cfa920a47ed9a1a968b1508ac85f1680c5ca4107
ShoreDroid <53381903+PACROM@users.noreply.github.com>
2019-09-08 21:13:11 -0400

    lets try it out

--- kernel.sh
+++ kernel.sh
@@ -8,6 +8,10 @@
 #
 # Copyright (C) 2019 by PACROM
 #
+
+#enter the kernel
+cd kernel/google/marlin
+
 echo "Let's begin by setting up the arch"
 
 #export arch
@@ -29,7 +33,7 @@ make mrproper
 make marlin_defconfig
 
 #make the kernel
-make
+make -j8
 
 echo "kernel build complete - now let's move it!"
 #remove the old one
@@ -37,5 +41,11 @@ rm -rf ~/pacrom/device/google/marlin-kernel/Image.lz4-dtb
 
 #copy the new one
 cp ~/pacrom/kernel/google/marlin/arch/arm64/boot/Image.lz4-dtb ~/pacrom/device/google/marlin-kernel
+
+#back to root
+cd .. & cd .. && cd ..
+
+#begin the build
+. build/envsetup.sh && lunch aosp_marlin-userdebug && m -j8
 
 
